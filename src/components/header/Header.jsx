e0aee7e090d87dc9e61660996aff2ea8e62b376f
ibrahim sannu <prince.ibrahim76@gmail.com>
2022-10-26 01:52:04 +0100

    updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,17 +8,21 @@ const Header = () => {
   return (
     <div id="profile__container">
       <div id="profile">
-        <div >
-          <img src={avatar} alt="avatar" id="profile__img"/>
+        <div>
+          <img src={avatar} alt="avatar" id="profile__img" />
         </div>
         <p id="twitter">ibrahimsannu</p>
         <p id="slack">prince.ibrahim76@gmail.com</p>
       </div>
       <div id="bg__icon">
-        <div id="web__img"> <img src={web} alt=""  id="web"/></div>
-        <div id="mobile__img"> <img src={mobile} alt=""  id="mobile"/></div>
-       
-       
+        <div id="web__img">
+          {" "}
+          <img src={web} alt="" id="web" />
+        </div>
+        <div id="mobile__img">
+          {" "}
+          <img src={mobile} alt="" id="mobile" />
+        </div>
       </div>
     </div>
   );
